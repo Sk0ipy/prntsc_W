a308c7c09f9e9b798276ed2a0da032e981f6737a
Sk0ipy <74152292+Sk0ipy@users.noreply.github.com>
2023-03-26 20:21:12 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+?>
+
+
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PrintScreen Saves</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+<main class="main">
+    <div class="title">
+        <h1>PrintScreen Saves</h1>
+    </div>
+
+
+
+
+
+
+</main>
+
+
+</body>
+</html>
